48e1180b728dad86052378fad09fbd5c479f865e
Huanye-Wang <138558319+Huanye-Wang@users.noreply.github.com>
2023-07-04 15:14:49 +0800

    Update SignupPage.jsx

--- linkup-frontend/src/Pages/SignupPage/SignupPage.jsx
+++ linkup-frontend/src/Pages/SignupPage/SignupPage.jsx
@@ -1,73 +1,73 @@
-import React, { useState } from "react";
-import "./Signup.css";
-import googleicon from "../../Assets/google-icon.svg";
-import SidebarImage from "../../Assets/SidebarImageSignup.jpg";
-import { useNavigate } from "react-router-dom";
-import { useGoogleLogin } from "@react-oauth/google";
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import SimpleCrypto from 'simple-crypto-js';
+进口 反应, { 使用状态 } 从 “做出反应”;
+进口 "./Signup.css ";
+进口 googleicon 从 "../../Assets/谷歌图标. svg ";
+进口 侧边图像 从 "../../Assets/SidebarImageSignup.jpg ";
+进口 { 使用导航 } 从 " react-路由器-dom ";
+进口 { useGoogleLogin } 从 " @react-oauth/google ";
+进口 axios 从 阿西奥斯的;
+进口 饼干 从 ' js-cookie ';
+进口 简单加密 从 '简单加密js ';
 
-export default function SignupPage() {
+出口 系统默认值 功能 签约() {
   
-  const secretKey = process.env.REACT_APP_CRYPTO_SECRET;
-  const crypto = new SimpleCrypto(secretKey);
-  const navigate=useNavigate();
+  常数 秘钥=过程。包封/包围（动词envelop的简写）.反应_应用_加密_秘密;
+  常数 秘密党员 = 新的简单加密(秘钥);
+  常数 航行=使用导航();
 
-  try{
-    const getcookies = Cookies.get('linkupdata')
-    const { email } = crypto.decrypt(getcookies);
+  尝试{
+    常数 获取getcookies=饼干。得到('链接数据')
+    常数 { 电子邮件 }=加密。解释(获取getcookies);
 
-    if(email){
-      navigate('/chat');
+    如果(电子邮件){
+      航行(/聊天');
     } 
-  } catch (err){
-    console.log(err);
+  } 捕捉 (犯罪){
+控制台。原木(犯罪);
   }
 
  
   
-  const [formData,setFormData] = useState({
-    fname:'',
-    lname:'',
-    email:'',
-    password:'',
+  常数 [表单数据,setFormData] = 使用状态({
+    fnname:'',
+    名字:'',
+    电子邮件:'',
+    密码:'',
   })
-  const [output,setOutput] = useState("");
-  const [loading, setLoading] = useState(false);
+  常数 [输出,设置输出] = 使用状态("");
+  常数 [装货, setLoading] = 使用状态(错误的);
 
-  function handleChange(e){
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  功能 遥控(e){
+    setFormData({...表单数据，[e.目标.名字]:e。目标.价值 });
   }
 
-  const handleSubmit = async (e) => {
-    setLoading(true);
-    if(!formData.fname || !formData.lname || !formData.email || !formData.password){
-      setOutput("all fields are required");
-      setLoading(false);
-      return;
+  常数 处理提交 = 异步ˌ非同步(asynchronous) (e) => {
+    setLoading(真实的);
+    如果(！表单数据。fnname|| !表单数据。名字|| !表单数据。电子邮件|| !表单数据。密码){
+      设置输出("所有字段都是必填的");
+      setLoading(错误的);
+      返回;
     }
-    if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(formData.email)) {
-      setOutput("please enter a valid email");
-      setLoading(false);
-      return;
+    如果(!/^[A-Z0-9._%+-]+@[A-Z0-9。-]+\.[A-Z]{2，}$/i.试验(表单数据。电子邮件)) {
+      设置输出("请输入有效的电子邮件地址");
+      setLoading(错误的);
+      返回;
     }
-    if(formData.password.length < 6){
-      setOutput("password must be at least 6 characters long");
-      setLoading(false);
-      return;
+    如果(表单数据。密码.长度 < 6){
+      设置输出("密码长度必须至少为6个字符");
+      setLoading(错误的);
+      返回;
     }
-    try {
-      //const response = await axios.post('http://localhost:3001/auth/signup', formData);
-      const response = await axios.post('https://linkup-backend-k05n.onrender.com/auth/signup', formData);
-      setLoading(false);
-      console.log(response);
+    尝试 {
+      //const response = await axios . post(' http://localhost:3001/auth/sign up '，formData)；
+      常数 反应 = 等待axios。邮政(' https://link up-back end-k05n . on render . com/auth/sign up '，表单数据);
+      setLoading(错误的);
+控制台。原木(反应);
 
-      if(!navigator.onLine){
-        setOutput("You are connected to internet")
+      如果(！领航员。在线的){
+        设置输出("您已连接到互联网")
       }
-      if(response.status === 200){
-        navigate("/login");
+      如果(回应。状态 === 200){
+        航行("/登录");
       }
       else if(response.status === 404){
         setOutput("Unable to connect to server");
@@ -147,6 +147,9 @@ export default function SignupPage() {
               <div className="spinner-border" style={{height:"20px",width:"20px"}}></div>       
             )}
           </button>
+          <div>
+            Already have an account <Link to={'/login'}>log in</Link>
+          </差异>
           <div className="or">or</div>
           <div className="google-signup-btn" onClick={googlelogin}>
             continue with Google
